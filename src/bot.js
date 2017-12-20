ddb4828a865b7eacbe2be70f98114893c37b46a9
Mick van der Meijde <mickvdmeijde@gmail.com>
2017-12-20 11:39:30 +0100

    fixed the db!del it will now perfectly delete messages

--- src/bot.js
+++ src/bot.js
@@ -129,13 +129,14 @@ client.on("message", async message =>{
 	}
 	//delete messages
 	if (command === `${prefix}del`){
-		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
-		if (args < 2) return message.channel.send ("an amount between 2 and 100 is required")
- 		if (!args.length === 0) return message.channel.send("You did not specify an amount");
-		message.channel.fetchMessages({limit: parseInt(args[0])}).then(messages => message.channel.bulkDelete(messages));
-		console.log(args)
- 		return;
-	}
+	        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
+	    if (args.some(isNaN)) return message.channel.send("The input is Not a Number");
+	    if (args[0] < 1 || args[0] > 99) return message.channel.send ("an amount between 1 and 99 is required")
+		if (args.length === 0) return message.channel.send("You did not specify an amount");
+	        message.channel.fetchMessages({limit: parseInt(args[0].trim()) + 1}).then(messages => message.channel.bulkDelete(messages));
+	        console.log(args)
+	         return;
+		}
 
 });
 client.login(settings.token);
