a37409f30e086de96a200e81211d347a1c1f0624
Mick van der Meijde <mickvdmeijde@gmail.com>
2017-12-20 12:16:15 +0100

    made clear what is being done

--- src/bot.js
+++ src/bot.js
@@ -134,7 +134,7 @@ client.on("message", async message =>{
 	    if (args[0] < 1 || args[0] > 99) return message.channel.send ("an amount between 1 and 99 is required")
 		if (args.length === 0) return message.channel.send("You did not specify an amount");
 	        message.channel.fetchMessages({limit: parseInt(args[0].trim()) + 1}).then(messages => message.channel.bulkDelete(messages));
-	        console.log(args)
+	        console.log("deleted", args, "messages")
 	         return;
 		}
 
