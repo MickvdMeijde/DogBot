7e46b82bc995953d55b8d01c4f2d1f4698f554d3
Mick van der Meijde <mickvdmeijde@gmail.com>
2019-02-01 21:49:49 +0100

    attempt at getting js switch to work

--- src/bot.js
+++ src/bot.js
@@ -28,126 +28,151 @@ client.on("message", async message =>{
 
 	if(!command.startsWith(prefix)) return;
 
-	//help command
-	if (command === `${prefix}help`) {
-		let embed = new Discord.RichEmbed()
-			.addField("Prefix:", "My prefix is db!")
-			.addField("Commands:", "db!help - The help page you are currently on \ndb!info - Info about me \nmore WIP")
-			.addField("Moderation Commands (i protecc uwu):", "db!mute [mention] - Mutes the selected user. \ndb!unmute [mention] - Unmutes the selected user. \ndb!del <number of messages> - Deletes the specified amount of messages.")
-			.addField("Dog Commands:", "db!sit - I can sit on command :3 \ndb!pet - I like getting pet :D \ndb!fetch - I can fetch like no other :3")
-		message.channel.send(embed);
+	if (command === `${prefix}`+addition);
 
-		return;
-	}
-	//info
-	if (command === `${prefix}info`) {
-		const githubEmote= client.emojis.get(414049712057090059);
-		let embed = new Discord.RichEmbed()
-			.setAuthor(`${client.user.username}`, "https://cdn.discordapp.com/avatars/385108268177162240/d892ad2abd2ede2621a4d54e25f5ee22.webp?size=256)")
-			.addField("Version", `${package.version}`, true)
-			.addField("Creator", `${package.author}`, true)
-			.addField("running on", `${client.guilds.size} servers`, true)
-			.addField("contribute", `${githubEmote} https://github.com/MickvdMeijde/DogBot`, true)
-		message.channel.send(embed);
+	switch(addition) {
 
-		return;
-	}
-	//dog commands
-	//sit
-	if (command === `${prefix}sit`) {
-		message.channel.send("*sits down and wags*");
+		//help command
+		case 'help':
+			let embed = new Discord.RichEmbed()
+				.addField("Prefix:", "My prefix is db!")
+				.addField("General Commands:", "db!help - The help page you are currently on \ndb!info - Info about me \nmore WIP")
+				.addField("Moderation", "db!help mod")
+				.addField("db!dog", "watch me do tricks ^w^",)
+			message.channel.send(embed);
 
-		return;
-	}
-	//pet
-	if (command === `${prefix}pet`) {
-		message.channel.send("*licks your hand and wags*");
+			return;
 
-		return;
-	}
-	//fetch
-	if (command === `${prefix}fetch`) {
-		message.channel.send("*happily brings back the thrown stick and puts it at your feet*");
+		//help lists
+		//moderation
+		case 'help' + ' ' + 'mod':
+			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
+			let embed = new Discord.RichEmbed()
+				.addField("Moderation Commands:", "db!mute [mention] - Mutes the selected user. \ndb!unmute [mention] - Unmutes the selected user. \ndb!del <number of messages> - Deletes the specified amount of messages.")
+			message.channel.send(embed);
 
-		return;
-	}
-	//boop
-	if (command === `${prefix}boop`){
-		let toBoop = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-		if(!toBoop) return message.channel.send("You did not say who you want to boop ^-^");
-		if(toBoop.id === message.author.id) return message.channel.send("So selfish of you not to share your boops")
-		if(toMute.id === message.client.id) return message.channel.send("You can't mute yourself")
-	}
+			return;
 
-	//moderating commands
-	//mute
-	if (command === `${prefix}mute`){
-		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
+		//dog commands
+		case 'help' + ' ' + 'dog':
+			let embed = new Discord.RichEmbed()
+				.addField("Dog Commands:", "db!sit - I can sit on command :3 \ndb!pet - I like getting pet :D \ndb!fetch - I can fetch like no other :3 \ndb!rollover - weeeee :D")
+			message.channel.send(embed);
 
-		let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-		if(!toMute) return message.channel.send("You did not specify a user");
+			return;
 
-		if(toMute.id === message.author.id) return message.channel.send("You can't mute yourself")
-		if(toMute.highestRole.position >= message.member.highestRole.position) return message.channel.send("You can't mute a member that has a higher role or has the same role as you")
+		//info
+		case 'info':
+			const githubEmote= client.emojis.get(414049712057090059);
+			let embed = new Discord.RichEmbed()
+				.setAuthor(`${client.user.username}`, "https://cdn.discordapp.com/avatars/385108268177162240/d892ad2abd2ede2621a4d54e25f5ee22.webp?size=256)")
+				.addField("Version", `${package.version}`, true)
+				.addField("Creator", `${package.author}`, true)
+				.addField("running on", `${client.guilds.size} servers`, true)
+				.addField("contribute", `https://github.com/MickvdMeijde/DogBot`, true)
+			message.channel.send(embed);
 
-		let role = message.guild.roles.find(r => r.name === "Muted");
-		if(!role) {
-			try{
-				role = await message.guild.createRole({
-					name: "Muted",
-					color: "#000000",
-					permissions: [],
-				});
+			return;
 
-				message.guild.channels.forEach(async (channel, id) => {
-					await channel.overwritePermissions(role, {
-						SEND_MESSAGES: false,
-						ADD_REACTIONS: false
-					});
-				});
-			} catch (e) {
-				console.log(e.stack);
-			}
-		}
+		//dog commands
+		//sit
+		case 'sit':
+			message.channel.send("*sits down and wags*");
 
-		if(toMute.roles.has(role.id)) return message.channel.send("This user is already muted");
+			return;
+		
+		//pet
+		case 'pet':
+			message.channel.send("*licks your hand and wags*");
 
-		await toMute.addRole(role);
-		message.channel.send("Muted");
+			return;
+		
+		//fetch
+		case 'fetch':
+			message.channel.send("*happily brings back the thrown stick and puts it at your feet*");
 
-		return;
+			return;
+		
+		//boop
+		case 'boop':
+			let toBoop = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+			if(!toBoop) return message.channel.send("You did not say who you want to boop ^-^");
+			if(toBoop.id === message.author.id) return message.channel.send("So selfish of you not to share your boops")
+			if(toBoop.id === client.id) return message.channel.send("AAHH my nows :O")
+			if(toBoop.id === message.mentions.users.first()) return message.channel.send("*boops*", message.mentions.users.first())
 
-	}
-	if (command === `${prefix}unmute`){
+		//roll over
+		case 'rollover':
+			message.channel.send("*rolls over and barks*")
+			
+
+		//moderating commands
+		//mute
+		case 'mute'
 			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
 
 			let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 			if(!toMute) return message.channel.send("You did not specify a user");
 
-			if(toMute.id === message.author.id) return message.channel.send("You can't unmute yourself")
+			if(toMute.id === message.author.id) return message.channel.send("You can't mute yourself")
 			if(toMute.highestRole.position >= message.member.highestRole.position) return message.channel.send("You can't mute a member that has a higher role or has the same role as you")
 
-
 			let role = message.guild.roles.find(r => r.name === "Muted");
+			if(!role) {
+				try{
+					role = await message.guild.createRole({
+						name: "Muted",
+						color: "#000000",
+						permissions: [],
+					});
 
-			if(!role || !toMute.roles.has(role.id)) return message.channel.send("This user is not muted");
+					message.guild.channels.forEach(async (channel, id) => {
+						await channel.overwritePermissions(role, {
+							SEND_MESSAGES: false,
+							ADD_REACTIONS: false
+						});
+					});
+				} catch (e) {
+					console.log(e.stack);
+				}
+			}
 
-			await toMute.removeRole(role);
-			message.channel.send("Unmuted");
+			if(toMute.roles.has(role.id)) return message.channel.send("This user is already muted");
+
+			await toMute.addRole(role);
+			message.channel.send("Muted");
 
 			return;
 
+		case 'unmute':
+				if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
+
+				let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+				if(!toMute) return message.channel.send("You did not specify a user");
+
+				if(toMute.id === message.author.id) return message.channel.send("You can't unmute yourself")
+				if(toMute.highestRole.position >= message.member.highestRole.position) return message.channel.send("You can't mute a member that has a higher role or has the same role as you")
+
+
+				let role = message.guild.roles.find(r => r.name === "Muted");
+
+				if(!role || !toMute.roles.has(role.id)) return message.channel.send("This user is not muted");
+
+				await toMute.removeRole(role);
+				message.channel.send("Unmuted");
+
+				return;
+
+		//delete messages
+		case 'del':
+		        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
+		    if (args.some(isNaN)) return message.channel.send("The input is Not a Number");
+		    if (args[0] < 1 || args[0] > 99) return message.channel.send ("an amount between 1 and 99 is required")
+			if (args.length === 0) return message.channel.send("You did not specify an amount");
+		        message.channel.fetchMessages({limit: parseInt(args[0].trim()) + 1}).then(messages => message.channel.bulkDelete(messages));
+		        console.log("deleted", args, "messages")
+		         return;
 	}
-	//delete messages
-	if (command === `${prefix}del`){
-	        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have the required permissions to execute this command")
-	    if (args.some(isNaN)) return message.channel.send("The input is Not a Number");
-	    if (args[0] < 1 || args[0] > 99) return message.channel.send ("an amount between 1 and 99 is required")
-		if (args.length === 0) return message.channel.send("You did not specify an amount");
-	        message.channel.fetchMessages({limit: parseInt(args[0].trim()) + 1}).then(messages => message.channel.bulkDelete(messages));
-	        console.log("deleted", args, "messages")
-	         return;
-		}
 
 });
 client.login(settings.token);
